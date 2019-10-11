672006b9d0e2c477c50ea48bbee9d4019e4a05af
kazy <kazy.dev@gmail.com>
2019-10-11 13:30:43 +0900

    add splach image. and add logo image in topView.

--- SwiftUI-Layout-Practice/SwiftUI-Layout-Practice/Views/TopView/TopView.swift
+++ SwiftUI-Layout-Practice/SwiftUI-Layout-Practice/Views/TopView/TopView.swift
@@ -15,9 +15,18 @@ struct TopView: View {
     
     var body: some View {
         NavigationView {
-            List(viewModel.list, id: \.id) { rowData in
-                NavigationLink(destination: rowData.presentType) {
-                    Text(rowData.title)
+            HStack {
+                VStack {
+                    Image("swiftui-layout_logo")
+                        .resizable()
+                        .frame(width: 120*10/4, height: 120)
+                        .aspectRatio(10/4, contentMode: .fit)
+                        .padding()
+                    List(viewModel.list, id: \.id) { rowData in
+                        NavigationLink(destination: rowData.presentType) {
+                            Text(rowData.title)
+                        }
+                    }
                 }
             }
             .navigationBarTitle("SwiftUI-Layout")
